31b08bb34c769ade857380f8542811668416ed73
Cristian Vasile Mocanu <cvmocanu@gmail.com>
2019-02-03 10:56:03 +0100

    main.ts: improve explanatory comments

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,9 @@ const os = require("os");
 
 // This will return a compiled template.
 //
-// A compiled template is a js function with the following signature: "function evaluateTemplate(model)",
+// A compiled template is a js function with the following signature: "function render(model: Object): string",
 // where "model" is any model that needs to be passed to the template.
+// The function will return a string with the evaluated template.
 const helloTemplate = require("./templates/hello.ejs");
 
 // the model used by our template
